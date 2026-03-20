616a7665f74c3b885e7ccff1ec606273fc9ce66c
Keith Keydel <keydelk@tuta.com>
2026-03-19 20:18:00 -0600

    Initial commit: set up cow-proj structure

---
+++ cowfucius.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Ask the wise Cowfucius for advice
+
+import cowsay
+import os
+from dotenv import load_dotenv
+from google import genai
---
+++ moo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Simple Cowsay program to test venvs
+from cowsay import cow
+
+cow("Mooo!")
